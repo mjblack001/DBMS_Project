4b2dddd74aa377a0e64a3d77bc024e9b6decc8b0
Tao Xie <taoxie1025@gmail.com>
2016-12-03 00:34:30 -0500

    Adding apply for tutor page

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 	
    define('DB_SERVER', 'localhost');
    define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', '');
+   define('DB_PASSWORD', 'root');
    define('DB_DATABASE', 'tutordb');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
